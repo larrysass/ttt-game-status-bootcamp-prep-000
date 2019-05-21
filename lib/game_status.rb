e56c05a0166b5caa75f4960b834b685772864eba
Larry Sass-Ainsworth <larry.sassainsworth@gmail.com>
2019-05-21 23:02:38 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -84,25 +84,6 @@ end
 end
    
 def winner(board)
-  
-  WIN_COMBINATIONS.each do|win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-    
-      position_1 = board[win_index_1]
-      position_2 = board[win_index_2]
-      position_3 = board[win_index_3]
-      
-      
-      if (position_1 == "X" && position_2 == "X" && position_3 == "X") 
-        return "X"
-      elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
-        return "O"
-      else 
-        return nil
-      end
-      
-  end 
+  won?(board)
 end 
   
