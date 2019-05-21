6feff9a4d08ccfdd884a5ca11c575a1ec5359402
Larry Sass-Ainsworth <larry.sassainsworth@gmail.com>
2019-05-21 23:12:24 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -96,6 +96,7 @@ def winner(board)
       
       if  (position_1 == "X" && position_2 == "X" && position_3 == "X") 
       return "X"
+      elsif (position_1 == "O" && position_2 == "O" && position_3 == "O") 
     end
   end
 
