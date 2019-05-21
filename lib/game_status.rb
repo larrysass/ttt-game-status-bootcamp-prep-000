158fbed8e98e4338d5c9ed3fb45d69d5cfc35e2f
Larry Sass-Ainsworth <larry.sassainsworth@gmail.com>
2019-05-21 22:56:09 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -99,10 +99,8 @@ WIN_COMBINATIONS.each do|win_combination|
       
       if  (position_1 == "X" && position_2 == "X" && position_3 == "X") 
         return "X"
-        
-        
-        (position_1 == "O" && position_2 == "O" && position_3 == "O")
-  
+      elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
+        return "O"
 end 
 end
 end 
